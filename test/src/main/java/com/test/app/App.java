6fa14859abd98dc556f536c30320563c6076f776
lyou0001 <lisa.j.young@gmail.com>
2014-04-23 21:11:27 +0200

    third question answered

--- test/src/main/java/com/test/app/App.java
+++ test/src/main/java/com/test/app/App.java
@@ -6,6 +6,9 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
 
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+
 import com.test.app.filemanager.FileManager;
 import com.test.app.objects.Person;
 
@@ -50,6 +53,8 @@ public class App  {
     	//answer second question
     	question2(persons);
     	
+    	//answer third question
+    	question3(persons);
     }
     
     private static void question1(Person[] persons) {
@@ -93,6 +98,40 @@ public class App  {
     		System.out.println("No one could be found");
     	}
     	
+    }
+    
+    private static void question3(Person[] persons) {
+    	
+    	System.out.println("\nHow many days are there between Bill and Paul?");
+    	
+    	Person bill = null;
+    	Person paul = null;
+    	
+    	for (Person p : persons) {
+    		if (p.getName().startsWith("Bill")) {
+    			bill = p;
+    		} else if (p.getName().startsWith("Paul")) {
+    			paul = p;
+    		}
+    	}
+    	
+    	if (bill!=null && paul!=null) {
+    		DateTime dt1 = null;
+    		DateTime dt2 = null;
+    		
+    		if (bill.getDob().after(paul.getDob())) {
+    			dt1 = new DateTime(paul.getDob());
+    			dt2 = new DateTime(bill.getDob());
+    		} else {
+    			dt1 = new DateTime(bill.getDob());
+    			dt2 = new DateTime(paul.getDob());
+    		}
+    		
+    		int days = Days.daysBetween(dt1, dt2).getDays();
+    		System.out.println("There are " + days + " days between Bill and Paul."); 
+    	} else {
+    		System.out.println("Could not find Bill or Paul.");
+    	}
     }
     
 }
